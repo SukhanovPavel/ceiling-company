076a266f3fff10bec1aadb73ebc8e3e319d588cb
Pavel Sukhanov <psuhanov77@gmail.com>
2023-07-30 00:50:51 +0300

    .

--- src/pages/ourWorks.tsx
+++ src/pages/ourWorks.tsx
@@ -7,7 +7,7 @@ import { WorkPrice } from "@/shared/WorkPrice/WorkPrice";
 const OurWorks = () => (
     <>
       <Layout data={dataHome}/>
-      <WorkPrice servicesMap={WorksPrice}  titleH1={description.home.ourWorks.title.toString()}/>
+      <WorkPrice servicesMap={WorksPrice}  titleH2={description.home.ourWorks.title.toString()}/>
     </>
 )
 
