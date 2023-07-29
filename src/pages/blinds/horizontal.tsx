e893b47022030ccf245b64f18ea4123ced6134a1
Pavel Sukhanov <psuhanov77@gmail.com>
2023-07-29 20:17:05 +0300

    fix data

--- src/pages/blinds/horizontal.tsx
+++ src/pages/blinds/horizontal.tsx
@@ -5,11 +5,22 @@ import { ProductPage } from "@/shared/ProductPage/ProductPage";
 import { description } from "@/assets/mocks/description";
 import { dataBlind } from "@/assets/mocks/blindPageData";
 
+
+const data = {
+  image: description.blinds.horizontal.image,
+  title: description.blinds.horizontal.title,
+  description: description.blinds.horizontal.description,
+  priority: {
+    title: description.blinds.horizontal.priority.title,
+    list: description.blinds.horizontal.priority.list
+  }
+}
+
 const Horizontal = () => {
   return (
     <div>
       <Layout data={dataBlind.links}/>
-      <ProductPage productData={description.blinds.horizontal}/>
+      <ProductPage productData={data}/>
     </div>
   );
 };
