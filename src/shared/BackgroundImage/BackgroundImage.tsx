b58e6dedd334ef61244eccd41bb07bd2db9173d6
Pavel Sukhanov <psuhanov77@gmail.com>
2023-07-29 20:08:06 +0300

    fix func

--- src/shared/BackgroundImage/BackgroundImage.tsx
+++ src/shared/BackgroundImage/BackgroundImage.tsx
@@ -1,4 +1,3 @@
-import {Form} from "@/shared/Form/Form";
 import {CeilingSets} from "@/shared/CeilingSet/CeilingSet";
 import {WorkPrice} from "@/shared/WorkPrice/WorkPrice";
 import { WorksPrice } from "@/assets/mocks/worksPrice";
@@ -10,9 +9,6 @@ import { dataCeilings } from "@/assets/mocks/ceilingsPageData";
 import { DescriptionList } from "@/shared/DescriptionList/DescriptionList";
 
 const servicesMap = WorksPrice;
-
-export const CHECK = <svg className={styles.check} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-    <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
 
 type Props = {
     alt: string;
@@ -29,16 +25,17 @@ export const BackgroundImage = ({
             <img
                 id="logoMain"
                 className={styles.logo}
-                src="https://4.downloader.disk.yandex.ru/preview/d2fcc3a96d265777342475e861eec973ececd9240fec3cd361192e2facb5be12/inf/xZxwoI1Xz-wOI-oUzz8X0_J2E61a7hMZpHjCkzZD6u230fcerraMcc0oUgcNnWeXbMY5dtShRkQbyiSlSD7K4g%3D%3D?uid=336597251&filename=logo.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=336597251&tknv=v2&size=2880x1358" alt="logo"
+                src="https://4.downloader.disk.yandex.ru/preview/d2fcc3a96d265777342475e861eec973ececd9240fec3cd361192e2facb5be12/inf/xZxwoI1Xz-wOI-oUzz8X0_J2E61a7hMZpHjCkzZD6u230fcerraMcc0oUgcNnWeXbMY5dtShRkQbyiSlSD7K4g%3D%3D?uid=336597251&filename=logo.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=336597251&tknv=v2&size=2880x1358"
+                alt="logo"
             />
             <div className={styles.title}>
                 <h2>ДЕЛАЕМ НАТЯЖНЫЕ ПОТОЛКИ</h2>
                 <p>ЧИСТЫМИ РУКАМИ С ЧИСТОЙ СОВЕСТЬЮ</p>
             </div>
-            <Form handleCloseModal={()=>{}} notFocus    />
+            {/*<Form handleCloseModal={()=>{}} notFocus    />*/}
         </div>
 
-        <CeilingSets handleClick={() => {}} />
+        {/*<CeilingSets handleClick={() => {}} />*/}
 
         <WorkPrice servicesMap={servicesMap} titleH1="Примеры стоимости натяжных потолков “под ключ”:" />
 
@@ -46,7 +43,9 @@ export const BackgroundImage = ({
 
         <DescriptionList alt={alt} img={src} />
 
-        <Contacts handleClick={() => {}} />
+        <Contacts
+          handleClick={() => null}
+        />
 
     </div>
 )
