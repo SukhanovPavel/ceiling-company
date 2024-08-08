0bb85855ed21260d454dab545c5fed50e16c3011
Pavel Sukhanov <psuhanov77@gmail.com>
2024-08-08 14:23:22 +0300

    add ya metrika

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ru">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+      <Main />
+      <NextScript />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(98022481, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                });
+              `
+        }}
+      />
+      <noscript>
+        <div><img src="https://mc.yandex.ru/watch/98022481" style="position:absolute; left:-9999px;" alt="" /></div>
+      </noscript>
       </body>
     </Html>
-  )
+  );
 }
